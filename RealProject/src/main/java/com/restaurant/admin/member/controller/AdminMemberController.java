2bcf1c703e868e5ede17c84540d02a492ae7bcee
이상훈 <00aaaa00@naver.com>
2018-03-14 16:25:55 +0900

    CONFLICT COMMIT

--- RealProject/src/main/java/com/restaurant/admin/member/controller/AdminMemberController.java
+++ RealProject/src/main/java/com/restaurant/admin/member/controller/AdminMemberController.java
@@ -2,11 +2,8 @@ package com.restaurant.admin.member.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
