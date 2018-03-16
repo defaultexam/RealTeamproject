cb3e5aa7eb59b9377fea3617ab0d9b135df3d138
이상훈 <00aaaa00@naver.com>
2018-03-16 10:08:36 +0900

    Member VO 수정 쿠폰 삭제

--- RealProject/src/main/java/com/restaurant/user/member/vo/MemberVO.java
+++ RealProject/src/main/java/com/restaurant/user/member/vo/MemberVO.java
@@ -23,15 +23,6 @@ public class MemberVO {
 	private String address = "";
 	private String memo = "";
 	private int point = 0;
-	private String b_coupon_no = "";
-	private Date b_coupon_start = null;
-	private Date b_coupon_end = null;
-	private String m_coupon_no = "";
-	private Date m_coupon_start = null;
-	private Date m_coupon_end = null;
-	private String v_coupon_no = "";
-	private Date v_coupon_start = null;
-	private Date v_coupon_end = null;
 	private Date member_outdate = null;
 	private int password_confirm = 0;
 
@@ -199,78 +190,6 @@ public class MemberVO {
 		this.point = point;
 	}
 
-	public String getB_coupon_no() {
-		return b_coupon_no;
-	}
-
-	public void setB_coupon_no(String b_coupon_no) {
-		this.b_coupon_no = b_coupon_no;
-	}
-
-	public Date getB_coupon_start() {
-		return b_coupon_start;
-	}
-
-	public void setB_coupon_start(Date b_coupon_start) {
-		this.b_coupon_start = b_coupon_start;
-	}
-
-	public Date getB_coupon_end() {
-		return b_coupon_end;
-	}
-
-	public void setB_coupon_end(Date b_coupon_end) {
-		this.b_coupon_end = b_coupon_end;
-	}
-
-	public String getM_coupon_no() {
-		return m_coupon_no;
-	}
-
-	public void setM_coupon_no(String m_coupon_no) {
-		this.m_coupon_no = m_coupon_no;
-	}
-
-	public Date getM_coupon_start() {
-		return m_coupon_start;
-	}
-
-	public void setM_coupon_start(Date m_coupon_start) {
-		this.m_coupon_start = m_coupon_start;
-	}
-
-	public Date getM_coupon_end() {
-		return m_coupon_end;
-	}
-
-	public void setM_coupon_end(Date m_coupon_end) {
-		this.m_coupon_end = m_coupon_end;
-	}
-
-	public String getV_coupon_no() {
-		return v_coupon_no;
-	}
-
-	public void setV_coupon_no(String v_coupon_no) {
-		this.v_coupon_no = v_coupon_no;
-	}
-
-	public Date getV_coupon_start() {
-		return v_coupon_start;
-	}
-
-	public void setV_coupon_start(Date v_coupon_start) {
-		this.v_coupon_start = v_coupon_start;
-	}
-
-	public Date getV_coupon_end() {
-		return v_coupon_end;
-	}
-
-	public void setV_coupon_end(Date v_coupon_end) {
-		this.v_coupon_end = v_coupon_end;
-	}
-
 	public Date getMember_outdate() {
 		return member_outdate;
 	}
@@ -285,20 +204,6 @@ public class MemberVO {
 
 	public void setPassword_confirm(int password_confirm) {
 		this.password_confirm = password_confirm;
-	}
-
-	@Override
-	public String toString() {
-		return "MemberVO [member_no=" + member_no + ", id=" + id + ", password=" + password + ", name=" + name
-				+ ", gender=" + gender + ", birthday=" + birthday + ", email=" + email + ", phone=" + phone
-				+ ", agreement1=" + agreement1 + ", agreement2=" + agreement2 + ", agreement3=" + agreement3
-				+ ", condition=" + condition + ", rank=" + rank + ", member_date=" + member_date + ", marriage="
-				+ marriage + ", weddingdate=" + weddingdate + ", job=" + job + ", address=" + address + ", memo=" + memo
-				+ ", point=" + point + ", b_coupon_no=" + b_coupon_no + ", b_coupon_start=" + b_coupon_start
-				+ ", b_coupon_end=" + b_coupon_end + ", m_coupon_no=" + m_coupon_no + ", m_coupon_start="
-				+ m_coupon_start + ", m_coupon_end=" + m_coupon_end + ", v_coupon_no=" + v_coupon_no
-				+ ", v_coupon_start=" + v_coupon_start + ", v_coupon_end=" + v_coupon_end + ", member_outdate="
-				+ member_outdate + ", password_confirm=" + password_confirm + "]";
 	}
 
 }
